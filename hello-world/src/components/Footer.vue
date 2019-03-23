a63b9fa809be5ff236c93c2896f67f7b813f3838
ChidinmaKO <kchidinmavictoria@gmail.com>
2019-03-23 03:53:16 +0100

    add basics and lifecycle hooks
    This commit also adds props, event bus, components using the vue CLI

---
+++ hello-world/src/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="footer">
+      <footer>
+          <p>{{ copyright }} {{ mainTitle }}.</p>
+      </footer>
+  </div>
+</template>
+
+<script>
+import { bus } from '../main.js';
+
+export default {
+  name: 'Footer',
+  props: {
+      mainTitle: {
+          type: String,
+          required: true
+      }
+  },
+  data: function() {
+    return{
+      copyright: 'Copyright 2019',
+    }
+  },
+  created() {
+      bus.$on('titleChanged', (data) => {
+          this.mainTitle = data;
+      })
+  },
+}
+</script>
+
+<style scoped>
+.footer {
+    color: #ffffff;
+    background: #283c86;
+    padding: 10px 0;
+}
+
+</style>
---
+++ hello-world/src/components/HelloNinjas.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="hello">
+    <h3>List of Ninjas and their specialties!</h3>
+    <ul>
+      <li v-for="(ninja, index) in ninjas" :key="index" @click="ninja.show = !ninja.show">
+        <h4>{{ ninja.name }} <span v-show="ninja.show">: {{ ninja.speciality }}</span></h4> 
+      </li>
+    </ul>
+    <button @click="deleteNinja">Delete Ninja</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloNinjas',
+  // props: ['ninjas'],
+  props: {
+    ninjas: {
+      type: Array,
+      required: true
+    }
+  },
+  data: function() {
+    return{
+    }
+  },
+  methods: {
+    deleteNinja: function() {
+      this.ninjas.pop()
+    }
+  },
+  beforeCreate() {
+    alert('beforeCreate');
+  },
+  created() {
+    // fetch data from APIs or database
+    alert('created');
+  },
+  beforeMount() {
+    alert('beforeMount');
+  },
+  mounted() {
+    // manipulate the dom
+    alert('mounted');
+  },
+  beforeUpdate() {
+    alert('beforeUpdate');
+  },
+  updated() {
+    alert('updated');
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  background: #DBDBDB;
+  padding: 30px 0;
+}
+h3 {
+  margin: 0;
+  color: maroon;
+}
+ul {
+  padding: 0;
+  margin: 0;
+}
+li {
+  cursor: pointer;
+}
+</style>
